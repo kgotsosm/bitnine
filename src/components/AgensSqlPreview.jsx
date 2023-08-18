90477f96bc9a0de5bc0e0cb17a3f6064305fee83
Kgotso Makhalimele <kgotsom@protonmail.com>
2023-08-18 20:47:38 +0200

    Abstracted AgensSQL preview component

---
+++ src/components/AgensSqlPreview.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import agens from "../assets/agenssql.png";
+
+const AgensSqlPreview = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center">
+        <img src={agens} alt="AgensSQL Logo" className="w-220 my-4" />
+      </div>
+      <div className="grid md:grid-cols-2 px-4 gap-8">
+        <div className="flex justify-space items-center gap-4 p-2 shadow-md">
+          <img
+            width="100"
+            height="100"
+            src="https://img.icons8.com/3d-fluency/188/security-checked.png"
+            alt="security-checked"
+          />
+          <div className="flex-flex-row">
+            <h3 className="font-semibold text-xl md:text-2xl text-background tracking-wider">
+              Data Security
+            </h3>
+            <p className="text-lg md:text-xl text-background">
+              Various features are added to ensure security and stability of
+              data{" "}
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-space items-center gap-4 p-2 shadow-md">
+          <img
+            width="100"
+            height="100"
+            src="https://img.icons8.com/3d-fluency/188/data-configuration.png"
+            alt="data-configuration"
+          />
+          <div className="flex-flex-row">
+            <h3 className="font-semibold text-xl md:text-2xl text-background tracking-wider">
+              High Availability
+            </h3>
+            <p className="text-lg md:text-xl text-background">
+              Standby server minimizes downtime by restoring the system
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className="flex justify-space items-center gap-4 p-2 shadow-md">
+            <img
+              width="100"
+              height="100"
+              src="https://img.icons8.com/3d-fluency/188/control-panel.png"
+              alt="control-panel"
+            />
+            <div className="flex-flex-row">
+              <h3 className="font-semibold text-xl md:text-2xl text-background tracking-wider">
+                Monitoring
+              </h3>
+              <p className="text-lg md:text-xl text-background">
+                Monitoring dashboard to track key metrics
+              </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="flex justify-space items-center gap-4 p-2 shadow-md">
+            <img
+              width="100"
+              src="https://img.icons8.com/3d-fluency/94/hub.png"
+              alt="hub"
+            />
+            <div className="flex-flex-row">
+              <h3 className="font-semibold text-xl md:text-2xl  tracking-wider">
+                Data Sharding
+              </h3>
+              <p className="text-lg md:text-xl text-background">
+                Distribute dataset across multiple databases
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AgensSqlPreview;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,21 +3,19 @@ import database from "../assets/database.webp";
 
 const Hero = () => {
   return (
-    <div className="w-full h-[90vh] bg-gray-100 flex flex-col justify-between ">
+    <div className="w-full h-[90vh] my-4 bg-background flex flex-col justify-between ">
       <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
-        <div className="flex flex-col justify-center md:items-start w-full px-2 py-4 gap-5 mx-3">
-          <h1 className="text-6xl md:text-8xl text-background font-bold font-inter">
+        <div className="flex flex-col justify-center w-full px-2 py-4 gap-5 mx-3">
+          <h1 className="text-6xl md:text-8xl text-titleText font-bold font-inter">
             Bitnine
           </h1>
-          <p className="text-teal-400 font-medium text-xl md:text-2xl py-2">
+          <p className="text-teal-200 font-medium text-xl md:text-2xl py-2">
             Home of the world's first RDB+GDB multi-model graph database product
           </p>
-          <button className="bg-blue-900 border-none py-3 my-2 rounded-md w-[300px] lg:w-[440px] hover:opacity-90">
-            Sign In
-          </button>
+          
         </div>
-        <div className="hidden md:block">
-          <img src={database} alt="Database" className="w-full" />
+        <div>
+          <img src={database} alt="Database" className="md:w-full rounded-sm shadow-blue-200 shadow-md"/>
         </div>
       </div>
     </div>
